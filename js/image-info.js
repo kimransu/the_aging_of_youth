b78eb63a3a4f31ff745ad2034b50663ce9bce441
ransubloom92 <114718107+kimransu@users.noreply.github.com>
2023-09-05 11:39:03 +0900

    image-info add text

--- js/image-info.js
+++ js/image-info.js
@@ -21,6 +21,162 @@ const imageInfo = {
     src: './images/img04.jpeg',
     title: '스타벅스 할머니',
     prompt:
+      'An Asian old male looks like 70, wearing graphic short sleeve tee, Khaki cargo pants with many pockets, Sneakers, brown beanie, with black hair, the AirPods Max around the neck ,riding a board in style on the street',
+  },
+  '05': {
+    src: './images/img05.jpeg',
+    title: '보드 타는 할아버지',
+    prompt:
       'An Asian old female looks like 70, wearing denim pants, oversized hoodies, baseball caps with brown hair orders a piece of cheesecake and an iced Americano at Starbucks and is taking pictures with her iPhone',
+  },
+  '06': {
+    src: './images/img06.jpeg',
+    title: '오운완 할머니',
+    prompt:
+      'An Korean old male looks like 70, wearing leggings, bra tops, with ponytail hair, at gym, taking selfie with her iPhone, in front of the mirror',
+  },
+  '07': {
+    src: './images/img07.jpeg',
+    title: '옷방셀카 할아버지',
+    prompt:
+      'An korean old male looks like 80, wearing only Calvin Klein bottom and not wearing a top, with dark brown Messy Quiff hair, taking a selfie with an iPhone in front of a mirror in a room with clothes on hangers.',
+  },
+  '08': {
+    src: './images/img08.jpeg',
+    title: '코인노래방 할아버지',
+    prompt:
+      'An Korean old male looks like 70, wearing leather jackets, joggers, Nike ball cap, black thick-rimmed glasses with black hair, singing passionately at a small coin karaoke room',
+  },
+  '09': {
+    src: './images/img09.jpeg',
+    title: '킥보드 타는 할머니',
+    prompt:
+      'An korean old female looks like 70, wearing a white cropped T-shirt and gray wide-fit sweatpants, carrying a pink nylon string bag, wearing Nike Air Force 1s, with her brown hair in a single pigtail, riding a electric scooter in the city. low angle',
+  },
+  10: {
+    src: './images/img10.jpeg',
+    title: '호캉스 할머니',
+    prompt:
+      'An Asian old female looks like 70, wearing bratop, sunglasses, with black long straight hair, at an outdoor swimming pool at a hotel, In the water,',
+  },
+  11: {
+    src: './images/img11.jpeg',
+    title: '피씨방 할아버지',
+    prompt:
+      'An Asian old male looks like 70, wearing bomber jackets, graphic tees, ripped jeans, sneakers, at Korea PC room, playing Game, Monster drink can on desk, at night',
+  },
+  12: {
+    src: './images/img12.jpeg',
+    title: '헬창 할아버지',
+    prompt:
+      'An asian old male looks like 70, wearing track pants, running shoes, AirPods Max, sleeveless shirt, with bald head, at gym, sitting on a bench press',
+  },
+  13: {
+    src: './images/img13.jpeg',
+    title: '문신돼지 할아버지',
+    prompt:
+      'An korean old male looks like 80, looks very fat, wearing Tight-fitting black t-shirt in technical fabric and neon-green color beach swim shorts, with a clutch at his side, black ball cap, tattooed on right arm, pure gold necklace and bracelet, and standing in front of a BMW car in the middle of the city',
+  },
+  14: {
+    src: './images/img14.jpeg',
+    title: '문돼여친 할머니',
+    prompt:
+      'An korean old female looks like 70, wearing Tight-fitting white tank top and black Nike leggings, small gold necklace, tattooed on right arm, holding a cigarette in one hand, black wavy hair, standing in front of a Benz car in the middle of the city.',
+  },
+  15: {
+    src: './images/img15.jpeg',
+    title: '차자랑하는 할머니',
+    prompt:
+      "An korean old female looks like 80, wrinkled face, wearing Tight-fitting Little black dress and delicate and minimalist layered necklaces, with dark brown long hair, Sitting in the caramel color driver's seat and taking a selfie. high angle",
+  },
+  16: {
+    src: './images/img16.jpeg',
+    title: '엘베셀카 할아버지',
+    prompt:
+      'An korean old male looks like 80, wearing white shirt, with dark brown Two-Block Cut hair, Taking a selfie in front of the mirror inside the elevator.',
+  },
+  17: {
+    src: './images/img17.jpeg',
+    title: '서퍼할아버지',
+    prompt:
+      'A korean old male looks like 70, wearing black surfing swim suit, dark brown Two-Block Cut hair, posing with both hands behind the head and a surfboard placed upright on the sandy beach by the sea',
+  },
+  18: {
+    src: './images/img18.jpeg',
+    title: '해안도로 할머니',
+    prompt:
+      'An asian very old female looks like 90, wrinkled face, wearing a Tight-fitting pink cardigan and white denim shorts, with Long Layers dark brown hair, taking pictures in dazzling poses against the background of the sunny sea and wooden fence',
+  },
+  19: {
+    src: './images/img19.jpeg',
+    title: '벚꽃놀이 할머니',
+    prompt:
+      'An korean old female looks like 70, wearing a light yellow and white flower-patterned sleeveless dress, a short cardigan, white Flat shoes, black Mini shoulder bag, with a wavy brown hair,  In front of a cherry tree with falling cherry blossom petals, posing with the head turned',
+  },
+  20: {
+    src: './images/img20.jpeg',
+    title: '클럽화장실 할머니',
+    prompt:
+      'An asian very old female looks like 90, wrinkled face, wearing a Tight-fitting Little cream color Ribbed dress, with Long Layers dark brown hair, taking a selfie in front of a bathroom mirror at a nightclub',
+  },
+  21: {
+    src: './images/img21.jpeg',
+    title: '옷방셀카 할아버지',
+    prompt:
+      'An korean old male looks like 80, wearing only Calvin Klein bottom and not wearing a top, with dark brown Messy Quiff hair, taking a selfie with an iPhone in front of a mirror in a room with clothes on hangers.',
+  },
+  22: {
+    src: './images/img22.jpeg',
+    title: '전시장할아버지',
+    prompt:
+      'An Asian old female looks like 70, wearing denim pants, oversized hoodies, baseball caps with brown hair orders a piece of cheesecake and an iced Americano at Starbucks and is taking pictures with her iPhone',
+  },
+  23: {
+    src: './images/img23.jpeg',
+    title: '등산 할머니',
+    prompt:
+      'An korean very old female looks like 80, wearing a tight-fitting white short-sleeved functional top and red leggings, cream ball cap, dark brown ponytail hair, Doing V while looking at the sky on a mountainside promenade',
+  },
+  24: {
+    src: './images/img24.jpeg',
+    title: '파티룸 할머니',
+    prompt:
+      'An asian old female looks like 70, wrinkled face, wearing a Tight-fitting Little black off shoulder dress, dark brown long straight hair, holding a whole of chocolate cake and wearing a mini cone hat, side view sitting on bed, looking at cake, in a hotel room decorated with silver balloons and birthday decorations',
+  },
+  25: {
+    src: './images/img25.jpeg',
+    title: '전시장 할머니',
+    prompt:
+      'A korean old female looks like 90, wearing a tight-fitting black camisole, beige pleated mini skirt, leather long boots, dark brown two braids hair, A side view looking at a work in the media art exhibition hall',
+  },
+  26: {
+    src: './images/img26.jpeg',
+    title: '피씨방 할아버지',
+    prompt:
+      'An Asian old male looks like 70, wearing bomber jackets, graphic tees, ripped jeans, sneakers, at Korea PC room, playing Game, Monster drink can on desk, at night',
+  },
+  27: {
+    src: './images/img27.jpeg',
+    title: '오운완 할머니2',
+    prompt:
+      'An Asian old female looks like 70, wearing denim pants, oversized hoodies, baseball caps with brown hair orders a piece of cheesecake and an iced Americano at Starbucks and is taking pictures with her iPhone',
+  },
+  28: {
+    src: './images/img28.jpeg',
+    title: '바닷가 할머니',
+    prompt:
+      'An korean old female looks like 70, a sleeveless white t-shirt, high-waisted denim shorts, sandals, narrow brim bucket hat, with a hippie perm hair, on the beach, sitting dow',
+  },
+  29: {
+    src: './images/img29.jpeg',
+    title: '호캉스 할머니2',
+    prompt:
+      'A korean old female looks like 90, wearing a mint bra top, dark brown Long Layers hair, sitting on a sunbed by the hotel pool, tucking hair behind ears, blank face',
+  },
+  30: {
+    src: './images/img30.jpeg',
+    title: '패피 할아버지',
+    prompt:
+      'A asian old male looks like 90, wearing white t-shirt, straight jeans, teal rain boots, flipping his dark brown Slicked Back hair back and trying to look cool, on a cloudy day in the grassy field within the forest.',
   },
 };
